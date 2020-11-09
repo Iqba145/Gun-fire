e7798f7a99a40257047c00da6c095d47c245bfb5
Iqbal Singh <IS011@visioncollege.ac.nz>
2020-11-09 14:16:36 +1300

    font sige change

--- Game_form.Designer.cs
+++ Game_form.Designer.cs
@@ -29,83 +29,87 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Game_form));
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
+            this.btn_Load = new System.Windows.Forms.Button();
+            this.btn_Spin = new System.Windows.Forms.Button();
+            this.btn_Shoot = new System.Windows.Forms.Button();
+            this.btn_Shootaway = new System.Windows.Forms.Button();
+            this.btn_Playagain = new System.Windows.Forms.Button();
+            this.btn_Exit = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // btn_Load
             // 
-            this.button1.BackColor = System.Drawing.Color.PeachPuff;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(12, 25);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(156, 103);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Load";
-            this.button1.UseVisualStyleBackColor = false;
+            this.btn_Load.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btn_Load.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Load.ForeColor = System.Drawing.Color.Red;
+            this.btn_Load.Location = new System.Drawing.Point(12, 25);
+            this.btn_Load.Name = "btn_Load";
+            this.btn_Load.Size = new System.Drawing.Size(156, 103);
+            this.btn_Load.TabIndex = 0;
+            this.btn_Load.Text = "Load";
+            this.btn_Load.UseVisualStyleBackColor = false;
             // 
-            // button2
+            // btn_Spin
             // 
-            this.button2.BackColor = System.Drawing.Color.PeachPuff;
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(12, 210);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(156, 110);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Spin";
-            this.button2.UseVisualStyleBackColor = false;
+            this.btn_Spin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btn_Spin.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Spin.ForeColor = System.Drawing.Color.Red;
+            this.btn_Spin.Location = new System.Drawing.Point(12, 210);
+            this.btn_Spin.Name = "btn_Spin";
+            this.btn_Spin.Size = new System.Drawing.Size(156, 110);
+            this.btn_Spin.TabIndex = 1;
+            this.btn_Spin.Text = "Spin";
+            this.btn_Spin.UseVisualStyleBackColor = false;
             // 
-            // button3
+            // btn_Shoot
             // 
-            this.button3.BackColor = System.Drawing.Color.PeachPuff;
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(12, 404);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(160, 103);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Shoot";
-            this.button3.UseVisualStyleBackColor = false;
+            this.btn_Shoot.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btn_Shoot.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Shoot.ForeColor = System.Drawing.Color.Red;
+            this.btn_Shoot.Location = new System.Drawing.Point(12, 404);
+            this.btn_Shoot.Name = "btn_Shoot";
+            this.btn_Shoot.Size = new System.Drawing.Size(160, 103);
+            this.btn_Shoot.TabIndex = 2;
+            this.btn_Shoot.Text = "Shoot";
+            this.btn_Shoot.UseVisualStyleBackColor = false;
             // 
-            // button4
+            // btn_Shootaway
             // 
-            this.button4.BackColor = System.Drawing.Color.PeachPuff;
-            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(259, 404);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(151, 103);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "Shoot away";
-            this.button4.UseVisualStyleBackColor = false;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.btn_Shootaway.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btn_Shootaway.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Shootaway.ForeColor = System.Drawing.Color.Red;
+            this.btn_Shootaway.Location = new System.Drawing.Point(259, 404);
+            this.btn_Shootaway.Name = "btn_Shootaway";
+            this.btn_Shootaway.Size = new System.Drawing.Size(151, 103);
+            this.btn_Shootaway.TabIndex = 3;
+            this.btn_Shootaway.Text = "Shoot away";
+            this.btn_Shootaway.UseVisualStyleBackColor = false;
+            this.btn_Shootaway.Click += new System.EventHandler(this.button4_Click);
             // 
-            // button5
+            // btn_Playagain
             // 
-            this.button5.BackColor = System.Drawing.Color.Green;
-            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(497, 404);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(156, 103);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "Play Again";
-            this.button5.UseVisualStyleBackColor = false;
+            this.btn_Playagain.BackColor = System.Drawing.Color.Green;
+            this.btn_Playagain.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Playagain.Location = new System.Drawing.Point(497, 404);
+            this.btn_Playagain.Name = "btn_Playagain";
+            this.btn_Playagain.Size = new System.Drawing.Size(156, 103);
+            this.btn_Playagain.TabIndex = 4;
+            this.btn_Playagain.Text = "Play Again";
+            this.btn_Playagain.UseVisualStyleBackColor = false;
             // 
-            // button6
+            // btn_Exit
             // 
-            this.button6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button6.Location = new System.Drawing.Point(739, 404);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(154, 103);
-            this.button6.TabIndex = 5;
-            this.button6.Text = "Exit";
-            this.button6.UseVisualStyleBackColor = false;
-            this.button6.Click += new System.EventHandler(this.button6_Click);
+            this.btn_Exit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btn_Exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Exit.Location = new System.Drawing.Point(739, 404);
+            this.btn_Exit.Name = "btn_Exit";
+            this.btn_Exit.Size = new System.Drawing.Size(154, 103);
+            this.btn_Exit.TabIndex = 5;
+            this.btn_Exit.Text = "Exit";
+            this.btn_Exit.UseVisualStyleBackColor = false;
+            this.btn_Exit.Click += new System.EventHandler(this.button6_Click);
             // 
             // pictureBox1
             // 
@@ -123,12 +127,12 @@
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(919, 512);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_Exit);
+            this.Controls.Add(this.btn_Playagain);
+            this.Controls.Add(this.btn_Shootaway);
+            this.Controls.Add(this.btn_Shoot);
+            this.Controls.Add(this.btn_Spin);
+            this.Controls.Add(this.btn_Load);
             this.Cursor = System.Windows.Forms.Cursors.Default;
             this.Name = "Game_form";
             this.Text = "Form1";
@@ -140,12 +144,12 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button btn_Load;
+        private System.Windows.Forms.Button btn_Spin;
+        private System.Windows.Forms.Button btn_Shoot;
+        private System.Windows.Forms.Button btn_Shootaway;
+        private System.Windows.Forms.Button btn_Playagain;
+        private System.Windows.Forms.Button btn_Exit;
         private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- Game_form.cs
+++ Game_form.cs
@@ -19,22 +19,28 @@ namespace GunShoot_game
 
         private void Game_form_Load(object sender, EventArgs e)
         {
-
+            btn_Spin.Enabled = false;
+            btn_Shoot.Enabled = false;
         }
 
         private void pictureBox2_Click(object sender, EventArgs e)
         {
+            btn_Spin.Enabled = true;
+            btn_Load.Enabled = false;
 
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-
+            btn_Shoot.Enabled = true;
+            btn_Spin.Enabled = false;
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
-
+            btn_Load.Enabled = true;
+            btn_Spin.Enabled = false;
+            btn_Shoot.Enabled = false;
         }
     }
 }
